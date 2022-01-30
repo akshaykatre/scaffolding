5947c2e03e8781d67b1f25baff07cd41f0b3eda5
Akshay Katre <akshaykatre@gmail.com>
2022-01-30 13:23:39 +0000

    Added subtraction

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 def add(x1, y1):
     return x1 + y1
 
+def subtract(x2, y2):
+    return abs(x2-y2)
 
 x = 1
 y = 2
 
 print("This is the sum {x}, {y}, {sum}".format(x=x, y=y, sum=add(x, y)))
+print("This is the difference {x}, {y}, {diff}".format(x=x, y=y, diff=subtract(x, y)))
